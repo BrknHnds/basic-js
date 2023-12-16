44dac8326ae0b2b3c9e0c3790ac7d4ba97e683da
BrknHnds <bsbd42@yandex.ru>
2023-12-17 01:19:54 +0500

    feat: add carbon-dating solution 2023-12-17 Sun 01:19:54

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,12 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || isNaN(parseFloat(sampleActivity)) || sampleActivity <= 0 || sampleActivity > MODERN_ACTIVITY) {
+    return false;
+  }
+  // ln(N0 / N) / k = ln(N0 / N) / (ln2 / t1/2) = (ln(N0 / N) * t1/2) / ln2
+  return Math.ceil((Math.log(MODERN_ACTIVITY / sampleActivity) * HALF_LIFE_PERIOD) / Math.LN2);
 }
 
 module.exports = {
